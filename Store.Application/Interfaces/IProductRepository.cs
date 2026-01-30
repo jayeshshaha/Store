2417d6909d35bc67aefde90d5a567ba1dc11b04c
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-01-30 23:09:25 +0530

    added react app

---
+++ Store.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Store.Domain.Entities;
+
+namespace Store.Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<(bool Exists, Product? Product)> GetProductAsync(int id);
+    }
+}
---
+++ Store.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces;
+using Store.Domain.Entities;
+using Store.Infrastructure.Data;
+
+namespace Store.Infrastructure.Repositories
+{
+    public class ProductRepository(StoreDbContext dbContext) : IProductRepository
+    {
+        public async Task<(bool Exists, Product? Product)> GetProductAsync(int id)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+            return (product != null, product);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await dbContext.Products.ToListAsync();
+        }
+    }
+}
+
+
