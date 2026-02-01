f02eb3c72147080d2ca5e8f766a45c4153d46dfb
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-02-01 13:04:59 +0530

    added mkcert package for https

--- store-ui/src/App.tsx
+++ store-ui/src/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+
 const App = () => {
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch("https://localhost:5001/api/products");
+      const data = await response.json();
+      console.log(data);
+
+    }
+
+    fetchProducts();
+
+  }, [])
+
   return (
     <div>App</div>
   )
